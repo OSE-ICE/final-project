3bd98256029a03a0f3d8e003407458c1d05a31ad
OSE-ICE <ose1975@gmail.com>
2024-05-02 15:11:50 +0000

    breytti file structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+"use client";
+import Header from "@/components/header";
 import HomePage from "./home/page";
+import "./globals.css";
 
 export default function Page() {
   return (
     <div>
+      <Header />
       <HomePage />
     </div>
   );
---
+++ src/components/header.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./header.css";
+import "../assets/logo.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="../assets/logo.png" alt="Logo" className="logo" />
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/dishes">Dishes</a>
+          </li>
+          <li>
+            <a href="/drinks">Drinks</a>
+          </li>
+          <li>
+            <a href="/orders">Orders</a>
+          </li>
+          <li>
+            <a href="/receipts">Receipt</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
