ce3baca033d01782acf8260c80241b2590aee89c
OSE-ICE <ose1975@gmail.com>
2024-05-02 18:32:06 +0000

    added containers to homepage and media queries

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,18 @@
+import React from "react";
+import "../globals.css";
+
 export default function HomePage() {
   return (
     <div>
       <h1>Home Page</h1>
       <p>Welcome to the home page!</p>
+      <img src="../assets/logo.png" alt="Logo" className="logo" />
+      <div className="container-wrapper">
+        <div className="container">Content of container 1</div>
+        <div className="container">Content of container 2</div>
+        <div className="container">Content of container 3</div>
+        <div className="container">Content of container 4</div>
+      </div>
     </div>
   );
 }
