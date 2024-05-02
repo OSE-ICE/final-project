1be11d7472f85ce061a90861d5f6e30bbba514e5
OSE-ICE <ose1975@gmail.com>
2024-05-02 19:19:00 +0000

    bætti við myndum

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import "../globals.css";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function HomePage() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the home page!</p>
-      <img src="../assets/logo.png" alt="Logo" className="logo" />
       <div className="container-wrapper">
-        <div className="container">Content of container 1</div>
+        <div className="container">
+          <Carousel>
+            <div>
+              <img src="./assets/hamburger.JPG" />
+              <p className="legend">Hamburger</p>
+            </div>
+            <div>
+              <img src="./assets/image2.png" />
+              <p className="legend">Legend 2</p>
+            </div>
+            <div>
+              <img src="./assets/image3.png" />
+              <p className="legend">Legend 3</p>
+            </div>
+          </Carousel>
+        </div>
         <div className="container">Content of container 2</div>
         <div className="container">Content of container 3</div>
         <div className="container">Content of container 4</div>
