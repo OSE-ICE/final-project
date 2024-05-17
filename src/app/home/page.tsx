2cd3c239549a6492e272ec1aecfb15c34236f117
OSE-ICE <ose1975@gmail.com>
2024-05-17 10:05:37 +0000

    added find order on home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,8 +6,35 @@ import hamburger from "../assets/hamburger.jpg";
 import steak from "../assets/steak.jpg";
 import pizza from "../assets/pizza.jpg";
 import Link from "next/link";
+import { useState, useContext } from "react";
+import { OrderContext } from "../components/OrderContext";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export default function HomePage() {
+  const { order, setOrder } = useContext(OrderContext);
+  const [email, setEmail] = useState("");
+
+  const router = useRouter(); // Access the router
+
+  const findOrder = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/api/order/${email}`
+      );
+      const order = response.data;
+      if (order) {
+        setOrder(order); // Update the order in the context
+        localStorage.setItem("order", JSON.stringify(order)); // Update the order in local storage
+        router.push("../dishes"); // Navigate to the dishes page
+      } else {
+        console.error(`Could not find order with email: ${email}`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <div className="container-wrapper">
@@ -35,8 +62,13 @@ export default function HomePage() {
         </div>
         <div className="container">
           <h2>Find your order</h2>
-          <input type="email" placeholder="Enter your e-mail" />
-          <button>Find</button>
+          <input
+            type="email"
+            placeholder="Enter your e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button onClick={findOrder}>Find</button>
         </div>
         <div className="container">
           <h2>Dish of the day</h2>
